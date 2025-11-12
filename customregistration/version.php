b071ba4233251e9bca740de4c6cb64717e4edad6
Steve <stkatso@gmail.com>
2025-11-12 18:22:19 +0200

    Create version.php main file

---
+++ customregistration/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_customregistration';
+$plugin->version = 2025111200;
+$plugin->requires = 2022041900; // Moodle 4.0+
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
