747e0d46955eddb70923d7cf39358ea82e7a84e1
Steve <stkatso@gmail.com>
2025-11-12 19:33:37 +0200

    Add localization file

---
+++ customregistration/lang/en/local_customregistration.php
@@ -0,0 +1,16 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Custom Registration';
+$string['registrationtitle'] = 'New User Registration';
+$string['email'] = 'Email Address';
+$string['firstname'] = 'First Name';
+$string['lastname'] = 'Last Name';
+$string['country'] = 'Country';
+$string['mobile'] = 'Mobile Number';
+$string['register'] = 'Register';
+$string['emailexists'] = 'This email is already registered';
+$string['invalidemail'] = 'Invalid email format';
+$string['invalidmobile'] = 'Invalid mobile number format';
+$string['registrationsuccess'] = 'Registration successful! Check your email for login credentials.';
+$string['registrationfailed'] = 'Registration failed. Please try again.';
